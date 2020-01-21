a37378b53ab93de3691591d3a040d2737122e7de
Wojciech Niekrasz <wojciech.niekrasz@gmail.com>
2020-01-21 13:50:00 +0100

    added clientName detecion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import re
 import json
 from dotenv import load_dotenv, find_dotenv
 from os import environ as env
@@ -24,7 +25,18 @@ def get_rules(access_token, audience):
     response = requests.get(audience + "rules", headers=headers)
 
     json_response = response.json()
-    print(json_response)
+    return json_response
+
+def find_client(rules):
+    retrieved_data = []
+    
+    pattern = r"(context.+\')"
+
+    for rule in rules:
+        m = re.findall(pattern, rule['script'])
+        if m:
+            print("search:", m)
+            
 
 if __name__ == "__main__":
 
@@ -39,4 +51,6 @@ if __name__ == "__main__":
 
     bearer_token = get_token(AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_AUDIENCE, AUTH0_DOMAIN)
     
-    get_rules(bearer_token, AUTH0_AUDIENCE)
+    fetched_rules = get_rules(bearer_token, AUTH0_AUDIENCE)
+    
+    find_client(fetched_rules)
