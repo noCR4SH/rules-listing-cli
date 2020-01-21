57bdf21e3329399cce50fb45697f7b900c21bf0f
Wojciech Niekrasz <wojciech.niekrasz@gmail.com>
2020-01-21 05:53:17 +0100

    Major cleanup

--- main.py
+++ main.py
@@ -5,25 +5,34 @@ from os import environ as env
 
 import constants
 
-ENV_FILE = find_dotenv()
-if ENV_FILE:
-    load_dotenv(ENV_FILE)
+def get_token(client_id, client_secret, audience, domain):
 
-AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
-AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
-AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
-AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
+    conn = http.client.HTTPSConnection(domain)
 
-conn = http.client.HTTPSConnection(AUTH0_DOMAIN)
+    payload = {'client_id': client_id, 'client_secret': client_secret, 'audience': audience, "grant_type": "client_credentials" }
+    json_data = json.dumps(payload)
 
-payload = {'client_id': AUTH0_CLIENT_ID, 'client_secret': AUTH0_CLIENT_SECRET, 'audience': AUTH0_AUDIENCE, "grant_type": "client_credentials" }
-json_data = json.dumps(payload)
+    headers = { 'content-type': "application/json" }
 
-headers = { 'content-type': "application/json" }
+    conn.request("POST", "/oauth/token", json_data, headers)
 
-conn.request("POST", "/oauth/token", json_data, headers)
+    res = conn.getresponse()
+    data = res.read()
+    dict_res = json.loads(data)
 
-res = conn.getresponse()
-data = res.read()
+    return dict_res
 
-print(data.decode("utf-8"))
+if __name__ == "__main__":
+
+    ENV_FILE = find_dotenv()
+    if ENV_FILE:
+        load_dotenv(ENV_FILE)
+
+    AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
+    AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
+    AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
+    AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
+
+    bearer_token = get_token(AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_AUDIENCE, AUTH0_DOMAIN)
+
+    print(bearer_token)
