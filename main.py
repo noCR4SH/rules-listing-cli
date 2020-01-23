6981a3bced91dfeea2a0cfb80496c9cd4d8892e4
Wojciech Niekrasz <wojciech.niekrasz@gmail.com>
2020-01-23 01:29:12 +0100

    improved search method

--- main.py
+++ main.py
@@ -27,27 +27,45 @@ def get_rules(access_token, audience):
     response = requests.get(audience + "rules", headers=headers)
 
     json_response = response.json()
+
+    for rule in json_response:
+        cleaned_script = re.sub(r"[^\w]", " ",  rule['script']).split()
+        rule['cleaned_script'] = cleaned_script
+
     return json_response
 
-def find_client(rules):
-    pattern = r"(context.+\')"
+def get_clients(access_token, audience):
+    print("Gettings clients....")
 
-    for rule in rules:
-        m = re.findall(pattern, rule['script'])
-        if m:
-            n = re.findall(r"'(.*?)'", m[0])
-            rule['client_name'] = n[0]
-        else:    
-            rule['client_name'] = "none"
+    headers = { 'authorization': access_token["token_type"] + " " + access_token["access_token"] }
+
+    response = requests.get(audience + "clients?fields=client_id%2Cname&include_fields=true", headers=headers)
+
+    json_response = response.json()
+    return json_response
+
+def find_client(rules, clients):
+
+    for client in clients:
+        for rule in rules:
+            if client['name'] in rule['cleaned_script'] or client['client_id'] in rule['cleaned_script']:
+                rule['client_name'] = client['name']
+                rule['client_id'] = client['client_id']
+            else:
+                rule['client_name'] = "none"
+                rule['client_id'] = "none"
+
+    for i in rules:
         try:
-            del rule['script']
+            del i['script']
+            del i['cleaned_script']
         except KeyError:
             print("Key not found")
 
-    return rules    
+    return rules
 
 def generate_csv(dict_data, filename):
-    csv_columns = ['id', 'enabled', 'name', 'order', 'stage', 'client_name']
+    csv_columns = ['id', 'enabled', 'name', 'order', 'stage', 'client_name', 'client_id']
 
     print("Generating CSV file...")
 
@@ -73,9 +91,9 @@ if __name__ == "__main__":
     bearer_token = get_token(AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_AUDIENCE, AUTH0_DOMAIN)
     
     fetched_rules = get_rules(bearer_token, AUTH0_AUDIENCE)
-    
-    final_data = find_client(fetched_rules)
-    
+    clients_data = get_clients(bearer_token, AUTH0_AUDIENCE)
+    final_data = find_client(fetched_rules, clients_data)
+
     csv_name = str(input("Provide name for the file: "))
 
     generate_csv(final_data, csv_name)
